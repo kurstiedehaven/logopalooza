25da8551c2037f907fe6368a056d63875a61d5de
Kurstie DeHaven-Vargas <kurstiedehaven@gmail.com>
2024-03-02 00:21:47 -0500

    image renders, styling now

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const generateSVG = (answers) => {
     const svgLogo = `
     <svg height="200" width="300" xmlns="http://www.w3.org/2000/svg>
         ${shape.render()}
-        <text x="10" y="20" font-family="monospace" font-size="30" fill="${answers.textColor}">${answers.companyName}</text>
+        <text x="100" y="50" font-family="monospace" font-size="30" fill="${answers.textColor}" text-anchor="middle">${answers.companyName}</text>
     </svg >`;
 
     fs.writeFileSync('logo.svg', svgLogo, (err) => {
